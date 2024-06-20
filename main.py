4f91300d41024a54622523294858824f867416e4
Felipe Buriticá <andres.ruizburitica@gmail.com>
2024-06-19 21:37:48 -0500

    Deploy on Render

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ import uvicorn
 from core.config import app
 
 
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
